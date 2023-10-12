104b92bc73ba0da1f3cff20bd79dfd55da35cecd
Varvara <varvara16030202@gmail.com>
2023-10-12 23:34:30 +0300

    My first commit

--- Practicum/sender_stand_request1.py
+++
@@ -1,7 +0,0 @@
-import configuration
-import requests
-
-def get_users_table():
-    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
-response = get_users_table()
-print(response.status_code)
